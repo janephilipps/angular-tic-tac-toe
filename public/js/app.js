c50295f9452ae040f09dbe7192d6298a8e920fce
Jane Philipps <jane.philipps@gmail.com>
2016-03-03 21:29:19 -0800

    get ng-repeat working, update CSS

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,19 @@ var app = angular.module('ticTacToeApp', []);
 
 app.controller('BoardCtrl', ['$scope', function($scope) {
 
-    $scope.test = 'hello';
+    $scope.board = {
+        rows: 3,
+        columns: 3,
+        board: [['X','X','X'],
+                ['X','X','X'],
+                ['X','X','X']]
+    };
 
 }]);
 
 app.directive('appBoard', function() {
   return {
-    templateUrl: '/views/board.html'
+    templateUrl: '/views/board.html',
+    controller: 'BoardCtrl'
   };
 });
