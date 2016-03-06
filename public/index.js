5124553dc6afce41f682801da1dea3b05a47b54a
Jane Philipps <jane.philipps@gmail.com>
2016-03-06 13:42:38 -0800

    install angular, get js loaded

--- public/index.js
+++ public/index.js
@@ -1,2 +1,14 @@
-var responsive = require('./css/responsive.css');
-var css = require('./css/style.css');
+var angular = require('angular');
+require('./css/responsive.css');
+require('./css/style.css');
+require('./js/controllers/HomeCtrl');
+require('./js/controllers/BoardCtrl');
+
+var app = angular.module('ticTacToeApp', ['HomeCtrl', 'BoardCtrl']);
+
+app.directive('appBoard', function() {
+  return {
+    template: require('./views/board.html'),
+    controller: 'BoardController'
+  };
+});
--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('ticTacToeApp', ['HomeCtrl', 'BoardCtrl']);
-
-app.directive('appBoard', function() {
-  return {
-    templateUrl: '/views/board.html',
-    controller: 'BoardController'
-  };
-});
