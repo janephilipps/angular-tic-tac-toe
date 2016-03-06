96e7c7b851ee11c2dd455af2e790a4c7dd4bbe9e
Jane Philipps <jane.philipps@gmail.com>
2016-03-06 11:20:33 -0800

    webpack config setup

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Entry point file
+    entry: './public',
+    // Compile bundle into builds directory under name bundle.js
+    output: {
+        path: 'builds',
+        filename: 'bundle.js'
+    }
+};
