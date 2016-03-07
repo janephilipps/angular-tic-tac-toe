593680696e4106aa0bbd47fedaaa338597771dfd
Jane Philipps <jane.philipps@gmail.com>
2016-03-06 21:28:57 -0800

    set up karma and jasmine for testing

---
+++ tests/tests.js
@@ -0,0 +1,12 @@
+describe('Calculator ', function() {
+
+// necessary
+it('should add two numbers correctly', function() {
+    expect(2).toEqual(1);
+});
+it('should subtract two numbers correctly', function() {});
+
+// helpful but not needed
+it('should add negative numbers', function() { });
+it('should reject non numbers', function() { });
+});
